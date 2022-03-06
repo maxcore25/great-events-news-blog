30aecb691d4f36a03b4b0d67bcdc543d53facb85
maxcore25 <mkorobov2002@mail.ru>
2022-03-06 09:32:57 +0300

    Get Form Data Function In Progress

--- layout/scripts/comment.js
+++ layout/scripts/comment.js
@@ -2,10 +2,24 @@ const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
 const commentInput = document.querySelector('#comment');
 const submitBtn = document.querySelector('form input[type="submit"]');
+const commentsField = document.querySelector('#comments ul');
+
+const commentTemplate = `
+`;
+let comments = [];
 
 submitBtn.addEventListener('click', e => {
   e.preventDefault();
   console.log(nameInput.value);
   console.log(emailInput.value);
   console.log(commentInput.value);
+
+  comments.push({
+    name: nameInput.value,
+    email: emailInput.value,
+    comment: commentInput.value,
+  });
+  console.log(comments);
+
+  commentsField.insertAdjacentHTML('beforeend');
 });
